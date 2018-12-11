c93aa60176a9977673a21c95ab8a971ccab235bc
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-12-11 16:12:17 -0500

    add query for recipes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,23 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/'
+})
+
+client
+  .query({
+    query: gql`
+      {
+        recipes {
+          id
+          title
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 class App extends Component {
   render() {
