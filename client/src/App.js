486bc987ae5302c16bcc58b973e6d2a50cc1b613
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-12-11 16:12:45 -0500

    add provider and consumer from apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import gql from 'graphql-tag';
+import { ApolloProvider, ApolloConsumer } from 'react-apollo';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/'
 })
 
-client
-  .query({
-    query: gql`
-      {
-        recipes {
-          id
-          title
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
-
 class App extends Component {
   render() {
     return (
-      <div>Hello World!</div>
+      <ApolloProvider client={client}>
+        <div>Hello World!</div>
+        <ApolloConsumer>
+          {client => {
+            client
+              .query({
+                query: gql`
+                  {
+                    recipes {
+                      id
+                      title
+                    }
+                  }
+                `
+              })
+              .then(result => console.log(result));
+              return null;
+          }}
+        </ApolloConsumer>
+      </ApolloProvider>
     );
   }
 }
